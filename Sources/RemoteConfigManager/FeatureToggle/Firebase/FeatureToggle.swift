ff0b2418eab80f7c451edac123a53ceed68e1c00
Luiz Hammerli <luizhammerli@gmail.com>
2022-12-10 10:17:03 -0300

    .

--- Sources/RemoteConfigManager/FeatureToggle/Firebase/FeatureToggle.swift
+++ Sources/RemoteConfigManager/FeatureToggle/Firebase/FeatureToggle.swift
@@ -10,13 +10,31 @@ import FirebaseRemoteConfig
 import Foundation
 
 public final class FeatureToggle: FeatureToggleProtocol {
-    public static func configure() {
-        FirebaseApp.configure()
-        let settings = RemoteConfigSettings()        
-        RemoteConfig.remoteConfig().configSettings = settings
+    let remoteConfigClient: RemoteConfig
+    
+    init(remoteConfigClient: RemoteConfig = RemoteConfig.remoteConfig()) {
+        self.remoteConfigClient = remoteConfigClient
     }
     
-    public func fetch<T>(key: String, completion: (T) -> Void) {
+    public static func configure(remoteConfigClient: RemoteConfig = RemoteConfig.remoteConfig()) {
+        FirebaseApp.configure()
+        let settings = RemoteConfigSettings()        
+        remoteConfigClient.configSettings = settings
+        remoteConfigClient.fetch { status, error in
+            if status == .success {
+                print("Success fetch remote config data from server")
+                remoteConfigClient.activate { _, _ in }
+            } else {
+                print("Error to fetch remote config data from server")
+            }
+        }
+    }
+    
+    public func fetch<T>(key: String, completion: (T?) -> Void) {
+        guard let value = remoteConfigClient.configValue(forKey: key).stringValue else {
+            return completion(nil)
+        }
         
+        print(value)
     }
 }
